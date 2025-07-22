c0ef210be4b4f97a6f0d60be699eee624786520a
nipun1a <nipun1a1b@gmail.com>
2025-07-22 22:50:17 +0530

    middleware

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 // this is the middleware
+app.use(express.json()); // to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // to parse URL-encoded bodies
 
 app.use((req, res, next) => {
   console.log(`Incoming request: ${req.method} ${req.url}`);
@@ -19,7 +21,9 @@ app.get('/profile', (req, res) => {
 })
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send('Something broke!'); // this shows an error messagee on the client
+  console.log('Error occurred:', err.message);// log the error message to the console
+  next(); // pass the error to the next middleware
 
 })
 app.listen(3000, () => {
